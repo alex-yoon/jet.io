386c85e7bbe116121a31cc2530dda50c06a0f324
Alex Yoon <yoon512@gmail.com>
2019-02-11 19:17:34 -0500

    api endpoint for lobby index

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
-  root 'homes#index'
   devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root 'homes#index'
+
+  namespace 'api' do
+    namespace 'v1' do
+      resources :lobbies, only: [:index]
+    end
+  end
+
 end
