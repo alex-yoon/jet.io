41ef86f6b6c89692657349a936835dac15687743
Alex Yoon <yoon512@gmail.com>
2019-02-13 21:29:22 -0500

    separate logic for fetching

--- app/javascript/react/containers/ChatContainer.js
+++ app/javascript/react/containers/ChatContainer.js
@@ -9,38 +9,69 @@ class ChatContainer extends Component {
       users: [],
       messages: [],
       my_user: {},
-      my_message: ""
+      my_message: "",
+      error: null
     }
+    this.fetchCurrentUser = this.fetchCurrentUser.bind(this)
+    this.fetchMessageHistory = this.fetchMessageHistory.bind(this)
+    this.initiateWebSocket = this.initiateWebSocket.bind(this)
     this.handleMessageChange = this.handleMessageChange.bind(this)
     this.handleMessageSubmit = this.handleMessageSubmit.bind(this)
   }
 
   componentDidMount() {
-    fetch(`/api/v1/lobbies/${this.props.lobby_id}/chat`, {
-      method: "GET",
-      headers: { 'Content-Type': 'application/json' }
+    this.fetchMessageHistory()
+    .then((messages) => {
+      this.setState({messages: messages})
     })
-      .then((response) => response.json())
-      .then((backlog) => {
-        this.setState({ messages: backlog })
-      })
-      .catch((error) => {
-        console.error(`Failed to get message history: ${error.message}`)
-      })
+    .catch((error) => {
+      console.error(error.message)
+    })
+  }
 
+  fetchCurrentUser() {
     fetch(`/api/v1/users/current`, {
       method: "GET",
       headers: { 'Content-Type': 'application/json' }
     })
-      .then((response) => response.json())
-      .then((user) => {
-        console.log(user);
-        this.setState({ my_user: user })
+    .then((response) => response.json())
+    .then((user) => {
+      this.setState({ my_user: user })
+    })
+    .catch((error) => {
+      console.error(`Failed to retrieve current user data: ${error.message}`)
+    })
+  }
+
+  fetchMessageHistory() {
+    return new Promise((resolve, reject) => {
+      fetch(`/api/v1/lobbies/${this.props.lobby_id}/chat`, {
+        method: "GET",
+        headers: { 'Content-Type': 'application/json' }
+      })
+      .then((response) => {
+        if (response.ok) {
+          return response.json()
+        }
+        else if (response.status == 403) {
+          throw new Error("User is not logged in")
+        }
+      })
+      .then((messages) => {
+        resolve(messages)
       })
       .catch((error) => {
-        console.error(`Failed to retrieve current user data: ${error.message}`)
+        if (error.message == "User is not logged in") {
+          this.setState({
+            error: <p><a href="/users/sign_up">Sign up</a> or <a href="/users/sign_in">log in</a> to access chat</p>
+          })
+        }
+        reject(Error(`Failed to get message history: ${error.message}`))
       })
+    })
+  }
 
+  initiateWebSocket() {
     try {
       App.ChatChannel = App.cable.subscriptions.create(
         {
@@ -60,7 +91,6 @@ class ChatContainer extends Component {
     catch (error) {
       console.error(`Failed to connect to chat channel: ${error.message}`)
     }
-
   }
 
   handleMessageChange(event) {
@@ -71,15 +101,21 @@ class ChatContainer extends Component {
     event.preventDefault()
     let payload = {
       message: {
-        body: this.state.my_message,
-        author: this.state.my_user
+        body: this.state.my_message
       }
     }
     App.ChatChannel.send(payload)
   }
 
   render() {
-    if (this.state.messages.length == 0) {
+    if (this.state.error != null) {
+      return(
+        <div className="panel">
+          {this.state.error}
+        </div>
+      )
+    }
+    else if (this.state.messages.length == 0) {
       return(
         <div className="panel">
           Loading...
